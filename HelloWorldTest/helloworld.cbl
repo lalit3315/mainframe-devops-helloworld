d228736fc6996a97fdb299c52a024de91188321a
Administrator <Administrator@gsi005>
2020-11-23 05:24:24 -0600

    Line#5 added now

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -9,5 +9,4 @@
            DISPLAY "added to test git and jenkins".
            DISPLAY "Push to Git and Trigger Jenkins".
            DISPLAY "Line#5 added".
-           DISPLAY "Line#6 added".
            STOP RUN.
