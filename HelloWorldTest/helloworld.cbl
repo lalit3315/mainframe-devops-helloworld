161e8634a8e36f384d85680f35ab108210e2a68d
lalit3315 <lalit.kapila@gmail.com>
2021-01-26 02:22:05 -0600

    Final check for back on track
    Change-Id: I20277033281f1483b6232c8fbe4eacfdc2483857

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -10,5 +10,5 @@
       *    DISPLAY "Line Number# 3 Added".
            DISPLAY "Line Number# 4 Added".
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
-           DISPLAY "SAMPLE155".
+           DISPLAY "We are now back on track".
            STOP RUN.
