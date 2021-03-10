c45581fe2488293edd36d24c69f73386e5ffd3e3
lkapila <lkapila@ITEM-S69222.emea.msad.sopra>
2021-03-10 16:12:06 +0530

    CH2021 commented demo v0.1

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -10,6 +10,7 @@
       *    DISPLAY "Line Number# 3 Added".
            DISPLAY "Line Number# 4 Added".
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
-           DISPLAY "Test run before demo v0.1".
+      *CH2021 comment out demo v0.1     
+      *     DISPLAY "Test run before demo v0.1".
            DISPLAY "Test run during demo 0.8".
            STOP RUN.
