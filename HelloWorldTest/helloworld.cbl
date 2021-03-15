121e303d615846622338c7f97ed535aa3b5a6ccc
armehra <armehra@ITEM-s73929.dhcp.noi3.in.sopra>
2021-03-15 17:05:43 +0530

    v0.3

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -15,4 +15,6 @@
            DISPLAY "Test run during demo 0.8".
       *CH2103 comment out demo v0.2
            DISPLAY "TEST RUN Before DEMO 0.2".
+      *CH2104 comment out demo v0.3
+           DISPLAY "ARADHYA MEHRA CHANGES DONE ON 15-03-2021 FOR V0.3".
            STOP RUN.
