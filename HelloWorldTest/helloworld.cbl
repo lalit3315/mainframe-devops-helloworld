ff09f1bf66d7a5ef0ce880edf1ff2c0942a4ee2e
armehra <armehra@ITEM-s73929.dhcp.noi3.in.sopra>
2021-03-23 15:46:35 +0530

    v0.3

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -12,4 +12,6 @@
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
       *CH102 comment line demo v0.1
       *    DISPLAY "Test run before demo v0.1 ".
+      *CH103 comment line demo v0.3
+      *    DISPLAY "Changes for version 0.3".      
            STOP RUN.
