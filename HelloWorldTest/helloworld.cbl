27c92e2464751cf424e34f635f9724f816ae681c
lalit3315 <lalit.kapila@gmail.com>
2021-03-23 00:04:51 -0500

    Test Run before Demo March 23

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -6,16 +6,11 @@
        PROCEDURE DIVISION.
            DISPLAY "Hello world!".
            DISPLAY "Lalit has done it".
-           DISPLAY "Lalit added this line -- git concu".
       *CH101 below line 3 commented
       *    DISPLAY "Line Number# 3 Added".
            DISPLAY "Line Number# 4 Added".
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
       *CH102 comment out demo v0.1
-      *     DISPLAY "Test run before demo v0.1".
+      *    DISPLAY "Test run before demo v0.1".
            DISPLAY "Test run during demo 0.8".
-      *CH2103 comment out demo v0.2
-           DISPLAY "TEST RUN Before DEMO 0.2".
-      *CH2104 comment out demo v0.3
-           DISPLAY "ARADHYA MEHRA CHANGES DONE ON 15-03-2021 FOR V0.3".
            STOP RUN.
