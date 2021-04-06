f839ac5db7a0f068cc09182962c4878f2c5e85cb
lalit3315 <lalit.kapila@gmail.com>
2021-04-06 02:49:24 -0500

    Test Run prep session

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -10,12 +10,6 @@
       *    DISPLAY "Line Number# 3 Added".
            DISPLAY "Line Number# 4 Added".
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
-      *CH102 comment line demo v0.1
+      *CH102 comment live demo v0.1
       *    DISPLAY "Test run before demo v0.1 ".
-      *CH103 comment line demo Final
-           DISPLAY "Test run Final Demo ".
-      *CH103 comment line demo v0.3
-      *    DISPLAY "Changes for version 0.3".
-      *CH103 comment line demo v0.3
-      *    DISPLAY "Changes for version 0.3".
            STOP RUN.
