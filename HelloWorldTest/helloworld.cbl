3d6984990f670bc3b7d245b4c25bd695f01a611a
lalit3315 <lalit.kapila@gmail.com>
2021-06-18 06:55:08 -0500

    Test after RDP down - 18th June

--- HelloWorldTest/helloworld.cbl
+++ HelloWorldTest/helloworld.cbl
@@ -8,14 +8,6 @@
            DISPLAY "Lalit has done it".
       *CH101 below line 3 commented
       *    DISPLAY "Line Number# 3 Added".
-      *CH105 Changes Start
-           DISPLAY "Demo Line Number# 1 Added".
-           DISPLAY "Demo Line Number# 2 Added".
-           DISPLAY "Demo Line Number# 3 Added".
-           DISPLAY "Demo Line Number# 4 Added".
-           DISPLAY "Demo Line Number# 5 Added".
-      *CH105 Changes End
-
            DISPLAY "Line Number# 4 Added".
            DISPLAY "Test Rocket Git ASCII to EBCDIC".
       *CH102 comment live demo v0.1
